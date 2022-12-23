9bc885ce23e7511c5a5cee85fc3fd04831c3216c
sohalkumar <nomoney2party@gmail.com>
2022-12-23 12:12:34 +0530

    added routes and ejs

--- app.js
+++ app.js
@@ -1,61 +1,32 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose")
-const UserDetail = require("./Schemas/login")
-const app = express();
+const ejs = require("ejs");
+const login = require(__dirname + "/routes/login.js")
+const register = require(__dirname + "/routes/register.js")
+const search = require(__dirname + "/routes/search.js")
+const app = express()
 
+mongoose.set('strictQuery', true)
+
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
-
-mongoose.connect("mongodb://127.0.0.1:27017/user-login")
-.then(app.listen(process.env.PORT || 3000, (req, res) => {
+mongoose.connect("mongodb://127.0.0.1:27017/caremap")
+.then(app.listen(process.env.PORT || 3000, () => {
     console.log("Server running on port 3000")
 }))
-    .catch((e) => {
+    .catch(e => {
         console.log(e)
     }
 )
 
-app.get("/login", (req, res) => {
-    res.sendFile(__dirname + "/public/html/login.html")
+app.get("/", (req, res) => {
+    res.render("index", {listItems : []})
 })
 
-app.post("/login", (req, res) => {
-    let userEmail = req.body.email
-    let userPass = req.body.password
-    UserDetail.find({ email: userEmail }, (err, data) => {
-        if (data.length == 0) {
-            res.sendFile(__dirname + "/public/html/loginfailure.html")
-        }
-        else if (data[0].email == userEmail
-            && data[0].password == userPass) {
-            res.sendFile(__dirname + "/public/html/index.html")
-        } else if (data[0].password != userPass) {
-            res.redirect("/login")
-        }
-    })
-})
-
-app.get("/register", (req, res) => {
-    res.sendFile(__dirname + "/public/html/register.html")
-})
-
-app.post("/register", (req, res) => {
-    UserDetail.find({email: req.body.email}, (err, data) => {
-        if (data.length != 0) {
-            res.sendFile(__dirname + "/public/html/registerfailure.html")
-        } else {
-            let details = new UserDetail({
-                email: req.body.email,
-                password: req.body.password
-            })
-            details.save()
-            res.sendFile(__dirname + "/public/html/index.html")
-        }
-    })
-})
-
-
-
+app.use("/login", login)
+app.use("/register", register)
+app.use("/search", search)
